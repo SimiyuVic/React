c9d5d14e0aab61d4b483cb050cff704ed7fe7610
Simiyu Victor <charlesvictor369@gmail.com>
2025-02-24 16:19:24 +0300

    Fixed error handling by displaying fetch errors and ensuring loading stops properly

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,10 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title }) => {
     return (
         <div className="blog-list">
             {blogs.map((blogs) => (
                 <div className="blog-preview" key={blogs.id}>
                     <h2>{blogs.title}</h2>
                     <p>Written by {blogs.author} </p>
-                    <button onClick={()=>handleDelete(blogs.id)}>Delete BLog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -3,24 +3,45 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const[blogs, setBlogs] = useState(null);
-    const handleDelete = (id) => 
-        {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const[isPending, setIsPending] = useState(true);
+    const[error, setError] = useState(null);  
     useEffect(()=>
     {
-        fetch('http://localhost:8001/blogs')
-            .then(res => {
+        setTimeout(() => 
+        {
+            fetch('http://localhost:8001/blogs')
+            .then(res => 
+            {    
+                if(!res.ok)
+                {
+                    throw Error('Could Not Fetch The Data For That Resource');
+                }
                 return res.json();
             })
-            .then(data => {
+            .then(data => 
+            {
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
             })
+            .catch(err => 
+            {
+                setIsPending(false);
+                setError(err.message);
+            })
+        }, 3000);
     },[]);
     return (
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title ="All Tittle !" handleDelete={handleDelete}/>}
+            {isPending && (
+                <div className="bubbles-loader">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            )}
+            {error && <div className="error">{error}</div>}
+            {blogs && <BlogList blogs={blogs} title ="All Tittle !"/>}
         </div>
     );
 }
